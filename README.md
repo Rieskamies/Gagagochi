9d6ef9c6cd37166284fbf12338a2a147d77207f6
Nico Lappalainen <t3lani00@students.oamk.fi>
2025-04-28 00:44:38 +0300

    Update

--- README.md
+++ README.md
@@ -22,13 +22,36 @@ Imitation of Tamagochi in JS
 - Made the site more flexible
 
 ### 0.22
+
+#### Majorish things
+
 - Added functions for all the stats to decrease
 - Updated feeding logic:
     -  Feeding now gives a little bit of energy
 - Implemented a pressable "Play" button
 - Added a minigame for "Play"
     -  Playing with the creature adds fun and decreases energy between 2 and 15
+
+#### Minor things
+
 - Made some sprites for action buttons
 
 ### 0.23
 - Added an animation for bathing
+
+### 0.33
+
+#### Majorish things
+
+- Added a penalty if you fail the minigame
+- Added eating animation
+- Added an animation for resting
+    - Resting is now functional and restores energy overtime while sleeping
+- Bathing is now implemented and functional
+    - Bathing restores cleanliness overtime while bathing
+
+#### Minor things
+
+- Minigame's "box" now appears on a random location based on viewport height and width
+- Cleaned up functions.js a bit
+- Fixed some bugs
--- js/actions.js
+++ js/actions.js
@@ -1,47 +1,64 @@
 
 const actions = document.querySelectorAll('.action');
+const actionImages = document.querySelectorAll('.action img');
+
 let clicked = false;
 console.log("button is not clicked " + clicked)
 
 actions.forEach((item, index) => {
   item.addEventListener('click', () => {
+
     //Feed
       if(index === 0 && !clicked ) {
           item.style.opacity = '20%';
-          clicked = true;
           console.log("button is clicked " + clicked)
           feedAction(item);
       }
-
         //Play
         if(index === 1 && !clicked ) {
           item.style.opacity = '20%';
-          clicked = true;
           console.log("button is clicked " + clicked)
           playAction(item);
       }
 
-        //Rest
+        //Bathe
         if(index === 2 && !clicked ) {
           item.style.opacity = '20%';
-          clicked = true;
           console.log("button is clicked " + clicked)
+          batheAction(item);
 
         }
 
-        //Bathe
+        //Sleep
         if(index === 3 && !clicked ) {
           item.style.opacity = '20%';
-          clicked = true;
           console.log("button is clicked " + clicked)
-
+          sleepAction(item);
           }
+
         //Cure
         if(index === 4 && !clicked ) {
           item.style.opacity = '20%';
-          clicked = true;
           console.log("button is clicked " + clicked)
 
           }
+  });
+});
+
+actionImages.forEach((item, index) => {
+  item.addEventListener('mouseenter', () => {
+    if (index === 0) item.src = './images/feedBtnPress.png';
+    if (index === 1) item.src = './images/playBtnPress.png';
+    if (index === 2) item.src = './images/batheBtnPress.png';
+    if (index === 3) item.src = './images/restBtnPress.png';
+    if (index === 4) item.src = './images/cureBtnPress.png';
+  });
+  
+  item.addEventListener('mouseleave', () => {
+    if (index === 0) item.src = './images/feedBtn.png';
+    if (index === 1) item.src = './images/playBtn.png';
+    if (index === 2) item.src = './images/batheBtn.png';
+    if (index === 3) item.src = './images/restBtn.png';
+    if (index === 4) item.src = './images/cureBtn.png';
   });
 });
